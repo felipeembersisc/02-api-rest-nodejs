076bdbb8851187d4fc6bd9501c835d398d57060d
felipeembersisc <felipe.embersisc@gmail.com>
2026-03-28 07:48:10 -0300

    feat: implement tests

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -57,39 +57,35 @@ export async function transactionsRoutes(app: FastifyInstance) {
     },
   )
 
-  app.post(
-    '/',
-    { preHandler: [checkSessionIdExists] },
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit']),
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
       })
+    }
 
-      const { title, amount, type } = createTransactionBodySchema.parse(
-        request.body,
-      )
+    await knexDb('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
 
-      let { sessionId } = request.cookies
-
-      if (!sessionId) {
-        sessionId = crypto.randomUUID()
-
-        reply.setCookie('sessionId', sessionId, {
-          path: '/',
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        })
-      }
-
-      await knexDb('transactions').insert({
-        id: crypto.randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1,
-        session_id: sessionId,
-      })
-
-      return reply.status(201).send()
-    },
-  )
+    return reply.status(201).send()
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
+import { app } from './app.ts'
 import { env } from './env/index.ts'
-import { transactionsRoutes } from './routes/transactions.ts'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionsRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running')
